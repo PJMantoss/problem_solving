f276b51b8da7fe9758f335ec729862a03acbb3ac
pjmantoss <joelptoss@gmail.com>
2020-06-17 14:57:34 +0100

    update plusMinus

--- plusMinus.js
+++ plusMinus.js
@@ -1,18 +1,28 @@
 function plusMinus(arr) {
-let result,
-    positive = 0,
-    negative = 0;
+let positive = 0,
+    negative = 0,
+    zero = 0;
+let result1, result2, result3;
 
     for (let i=0; i<arr.length; i++){
         if(arr[i] > 0){
             positive++;
-            result = positive / arr.length;
+            result1 = positive / arr.length;
         }
         if(arr[i] < 0){
             negative++;
-            result = negative / arr.length;
+            result2 = negative / arr.length;
         }
-        if(arr[i] == 0)
+        if(arr[i] == 0){
+            zero++;
+           result3 = zero / arr.length;
+        }
     }
 
+    console.log(result1);
+    console.log(result2);
+    console.log(result3);
+
 }
+
+plusMinus([-4, 3, -9, 0, 4, 1])
