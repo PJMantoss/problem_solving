d12c86b6bccf99a0c3678d513ca064c2f9e9ecb1
pjmantoss <joelptoss@gmail.com>
2020-06-17 14:32:31 +0100

    update diagonalDiff

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,13 +1,13 @@
 function diagonalDifference(arr){
-    let output = 0, 
+    let output = 0, //Final result variable
         diagOne = 0, //Input matrix
         diagTwo = 0;
     
     //Loop through array to access diagonals
     for (let i=0; i<arr.length; i++){
-        //Calculating the primary diagonal
+        //Calculate the outer array
         diagOne += arr[i][i];
-        //
+        //Reverse and calculate the inner array
         diagTwo += arr[arr.length -1 -i][i]
     }
 
