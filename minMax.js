f1fbf2a6759f28d902f43b1b98a4cc9a89ce51b9
pjmantoss <joelptoss@gmail.com>
2020-06-17 19:42:20 +0100

    update miniMax

--- minMax.js
+++ minMax.js
@@ -1,20 +1,23 @@
 function miniMaxSum(arr) {
-    let mini = 0;
-    let max = 0;
-    let popV = arr.pop();
-   
+    let mini = 0; //Placeholder for minimum value
+    let max = 0; //Placeholder for maximum value
+    arr.sort((a,b) => a - b); //Sort elements in ascending order
+    let popV = arr.pop(); //Remove the last element in order to calculate minimum value
+
+    //Loop through the remaining elements and sum up to get minimum value
     for (let i=0; i<arr.length; i++){
         mini += arr[i];
     }
 
-    arr.push(popV);
-    arr.shift();
-
+    arr.push(popV); //Put back the last element
+    arr.shift(); //Remove the first element in order to calculate maximum value
+    
+    //Loop through the remaining elements and sum up to get maximum value
     for (let j=0; j<arr.length; j++){
         max += arr[j];
     }
-
+//Print the minimum and maximum values
    console.log(mini + " " + max);
 }
 
-miniMaxSum([1, 2, 3, 4, 5])
+miniMaxSum([7, 69, 2, 221, 8974])
