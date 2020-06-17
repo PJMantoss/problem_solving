c6851a4ba216f23f4658844c8f696af917cacae4
pjmantoss <joelptoss@gmail.com>
2020-06-17 18:24:11 +0100

    update staircase

--- staircase.js
+++ staircase.js
@@ -1,12 +1,13 @@
 //Write a program that prints a staircase of size n.
 function staircase(n) {
+    //
     let stepsArray = new Array(n).fill(' ');
-
+    //
     while(stepsArray.indexOf(' ') !== -1){
-        stepsArray.shift();
-        stepsArray.push('#');
-        console.log(stepsArray.join(''));
-        n--;
+        stepsArray.shift();//
+        stepsArray.push('#');//
+        console.log(stepsArray.join('')); //
+        n--; //
     }
 }
 
