753a99cd25fd0e8bec24e0021570f797ac03c9cd
pjmantoss <joelptoss@gmail.com>
2020-06-17 13:00:45 +0100

    update aVeryBigSum

--- aVeryBigSum.js
+++ aVeryBigSum.js
@@ -18,12 +18,15 @@ Output Format
 Print the integer sum of the elements in the array.
 */
 function aVeryBigSum(ar) {
-    let output;
+    let output = [], sum = 0;
 
     for (let i=0; i<ar.length; i++){
-        output = ar[i] + ar[i + 1];
+        sum += ar[i] + ar[i + 1];
+        output.push(sum);
     }
 
     return output;
 
 }
+
+aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
