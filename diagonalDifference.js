87f677c3a35dc3c64c59928d0a29f3e153f3d0b0
pjmantoss <joelptoss@gmail.com>
2020-06-17 14:24:05 +0100

    update diagonalDiff

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,5 +1,12 @@
 function diagonalDifference(arr){
-    let output = 0;
+    let output = 0, diagOne = 0, diagTwo = 0;
+
+    for (let i=0; i<arr.length; i++){
+        diagOne += arr[i][i];
+        diagTwo += arr[i -1 -i][i]
+    }
+
+    output = Math.abs(diagOne - diagTwo);
 
     return output;
 }
