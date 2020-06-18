2da418575e6041f9328c2371226c2c91174fbb5c
pjmantoss <joelptoss@gmail.com>
2020-06-18 15:31:44 +0100

    update timeConversion

--- timeCoversion.js
+++ timeCoversion.js
@@ -1,6 +1,21 @@
 function timeConversion(s) {
-    /*
-     * Write your code here.
-     */
+    let output = [];
+
+    let newStr = s.split(':');
+    let lastElem = newStr.pop();
+    lastElem.split('/[A-Z]/')[0].trim();
+
+    newStr.push(lastElem);
+
+    let newString = Number(newStr);
+
+    if(newString[0] == 12){
+        newString[0] - 12;
+        output.push(newString);
+    } else {
+        newString[0] + 12;
+    }
+
+    return output.join(':');
 
 }
