2f73851e19779ac63add1a1fc0909452b3645d0a
pjmantoss <joelptoss@gmail.com>
2020-06-18 14:56:08 +0100

    update minMax and staircase

--- staircase.js
+++ staircase.js
@@ -1,8 +1,9 @@
-//Write a program that prints a staircase of size n.
+//Write a program that prints a staircase of size n using #.
+
 function staircase(n) {
     //Create an array of n length with empty space for each array item
     let stepsArray = new Array(n).fill(' ');
-    //For each loop push the single-space out and add the # to replace them
+    //For each loop, push the single-space out and add the # symbol to replace them
     while(stepsArray.indexOf(' ') !== -1){
         stepsArray.shift();//Remove the first element from the array using .shift()
         stepsArray.push('#');//Push the # symbol to the end of the array
