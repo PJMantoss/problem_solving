ab45b6b7d0f13c9c05f543f9cdaaa66c8f8542e7
pjmantoss <joelptoss@gmail.com>
2020-06-18 13:53:50 +0100

    update compareTriplets

--- compareTriplets.js
+++ compareTriplets.js
@@ -1,7 +1,7 @@
 /* COMPARE TRIPLETS
 Alice and Bob each created one problem for HackerRank. 
 A reviewer rates the two challenges, awarding points on a scale 
-from  to  for three categories: problem clarity, originality, and difficulty.
+from 1 to 3 for three categories: problem clarity, originality, and difficulty.
 
 We define the rating for Alice's challenge to be the triplet a = [1,2,3], 
 and the rating for Bob's challenge to be the triplet b = [3,2,1].
