241af07a933f40ee427f02799dc54975bcee0890
pjmantoss <joelptoss@gmail.com>
2020-06-18 14:08:09 +0100

    update aVeryBigSum

--- aVeryBigSum.js
+++ aVeryBigSum.js
@@ -1,9 +1,11 @@
 /* A VERY BIG SUM
-Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+Calculate and print the sum of the elements in an array, 
+keeping in mind that some of those integers may be quite large.
 
 Function Description
 
-Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+Complete the aVeryBigSum function in the editor below. 
+It must return the sum of all array elements.
 
 aVeryBigSum has the following parameter(s):
 
