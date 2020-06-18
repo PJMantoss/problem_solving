8f4b88d074c85ff6e6640a52a9d19182bfe087a0
pjmantoss <joelptoss@gmail.com>
2020-06-18 13:52:50 +0100

    update compareTriplets

--- compareTriplets.js
+++ compareTriplets.js
@@ -1,9 +1,13 @@
 /* COMPARE TRIPLETS
-Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from  to  for three categories: problem clarity, originality, and difficulty.
+Alice and Bob each created one problem for HackerRank. 
+A reviewer rates the two challenges, awarding points on a scale 
+from  to  for three categories: problem clarity, originality, and difficulty.
 
-We define the rating for Alice's challenge to be the triplet , and the rating for Bob's challenge to be the triplet .
+We define the rating for Alice's challenge to be the triplet a = [1,2,3], 
+and the rating for Bob's challenge to be the triplet b = [3,2,1].
 
-Your task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], a[2] and  with b[2].
+Your task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1],
+and b[2] with b[2].
 
 If a[i] > b[i], then Alice is awarded  point.
 If a[i] < b[i], then Bob is awarded  point.
@@ -12,11 +16,15 @@ Comparison points is the total points a person earned.
 
 Given a and b, determine their respective comparison points.
 
-For example, a = [1,2,3] and b = [3,2,1]. For elements 0, Bob is awarded a point because a[0] < b[0]. For the equal elements  and , no points are earned. Finally, for elements ,  so Alice receives a point. Your return array would be  with Alice's score first and Bob's second.
+For example, a = [1,2,3] and b = [3,2,1]. For elements 0, Bob is awarded 
+a point because a[0] < b[0]. For the equal elements  and , no points are earned. 
+Finally, for elements ,  so Alice receives a point. Your return array would be  
+with Alice's score first and Bob's second.
 
 Function Description
 
-Complete the function compareTriplets in the editor below. It must return an array of two integers, the first being Alice's score and the second being Bob's.
+Complete the function compareTriplets in the editor below. 
+It must return an array of two integers, the first being Alice's score and the second being Bob's.
 */
 
 function compareTriplets(a, b) {
