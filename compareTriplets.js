76daea6476a1d59e26789fbe16ca9bb175884a20
pjmantoss <joelptoss@gmail.com>
2020-06-18 13:56:11 +0100

    update compareTriplets

--- compareTriplets.js
+++ compareTriplets.js
@@ -7,7 +7,7 @@ We define the rating for Alice's challenge to be the triplet a = [1,2,3],
 and the rating for Bob's challenge to be the triplet b = [3,2,1].
 
 Your task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1],
-and b[2] with b[2].
+and a[2] with b[2].
 
 If a[i] > b[i], then Alice is awarded  point.
 If a[i] < b[i], then Bob is awarded  point.
@@ -17,8 +17,8 @@ Comparison points is the total points a person earned.
 Given a and b, determine their respective comparison points.
 
 For example, a = [1,2,3] and b = [3,2,1]. For elements 0, Bob is awarded 
-a point because a[0] < b[0]. For the equal elements  and , no points are earned. 
-Finally, for elements ,  so Alice receives a point. Your return array would be  
+a point because a[0] < b[0]. For the equal elements 1 and 1, no points are earned. 
+Finally, for elements 2,  so Alice receives a point. Your return array would be  
 with Alice's score first and Bob's second.
 
 Function Description
