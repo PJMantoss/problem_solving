1ee5a873145c8ab0f7b75f500d9f7671ac1d3b64
pjmantoss <joelptoss@gmail.com>
2020-06-20 19:50:06 +0100

    update countApples&Oranges

--- appleAndOrange.js
+++ appleAndOrange.js
@@ -3,14 +3,14 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     Loop through the apples array to access each element. If the value of an
     apple is greater than or equal to the difference between the s (starting of the houe) and
     a (position of apple tree) but is less than the difference between 
-    t (ending of the houe) and a print the length of the array
+    t (ending of the houe) and a, print the length of the array
     */
     console.log(apples.filter(f => s - a <= f && f <= t - a).length);
     /*
     Loop through the oranges array to access each element. If the value of an
-    apple is greater than or equal to the difference between the s (starting of the houe) and
-    a (position of apple tree) but is less than the difference between 
-    t (ending of the houe) and a print the length of the array
+    orange is greater than or equal to the difference between the s (starting of the houe) and
+    b (position of orange tree) but is less than the difference between 
+    t (ending of the houe) and b, print the length of the array
     */
     console.log(oranges.filter(fr => s - b <= fr && fr <= t - b).length);
 }
