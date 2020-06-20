1439634a5bebdbd570c89f698abda1fb4b23dffc
pjmantoss <joelptoss@gmail.com>
2020-06-20 19:42:47 +0100

    update countApples&Oranges

--- appleAndOrange.js
+++ appleAndOrange.js
@@ -1,4 +1,6 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-
-
+    console.log(apples.filter(f => s - a <= f && f <= t - a).length);
+    console.log(oranges.filter(fr => s - b <= fr && fr <= t - b).length);
 }
+
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])
