d4da5c10de42700fe37dadb8f0ab1efa9b28b4cd
pjmantoss <joelptoss@gmail.com>
2020-06-21 14:07:54 +0100

    update kangaroo

--- kangaroo.js
+++ kangaroo.js
@@ -1,3 +1,8 @@
+/*
+Complete the function kangaroo in the editor below. 
+It should return YES if they reach the same position 
+at the same time, or NO if they don't.
+*/
 function kangaroo(x1, v1, x2, v2) {
     if ((x1 - x2) % (v2 - v1)=== 0 && !(x2 > x1 && v2 > v1)){
         return "YES";
