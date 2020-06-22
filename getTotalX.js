b493c7707c8d480e22fa27fa03a7e754f64313db
pjmantoss <joelptoss@gmail.com>
2020-06-22 13:53:44 +0100

    update getTotalX

--- getTotalX.js
+++ getTotalX.js
@@ -1,4 +1,16 @@
 function getTotalX(a, b) {
-    // Write your code here
+    let count = 0,
+        factor;
+
+        for (let i=0; i<a.length; i++){
+            
+            for (let j=0; j<b.length; j++){
+                if(factor % a[i] === 0 && b[i] % factor === 0){
+                    count++;
+                }
+            }
+        }
+
+        return count;
 
 }
