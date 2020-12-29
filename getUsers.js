e38dc77c27a13dcacc9c7e64070e8081fbd6dadf
pjmantoss <joelptoss@gmail.com>
2020-12-29 09:58:44 +0100

    update getUsers

--- getUsers.js
+++ getUsers.js
@@ -2,3 +2,15 @@
 Call on JSON placeholder api, get 10 users, implement a function getUsers() that returns
 only names and emails of the users whose name contains letter k.
 */
+
+const getUsers = async () => {
+    const api = 'https://jsonplaceholder.typicode.com/users';
+    const response = await fetch(api);
+    const json = await response.json();
+
+    const result = json.filter(v >= v.name.toLowerCase().includes('k'));
+
+    console.log(result);
+}
+
+getUsers();
